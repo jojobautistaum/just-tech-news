657bcd283fc7e85b0fea31e700c66ed275a2f5e0
Jojo Bautista <gjojob@yahoo.com>
2022-01-10 14:57:58 -0600

    added stylesheets and middleware static

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const path = require('path');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
+
+// Middleware take all contents of a folder and serve them as static assets.
+// Useful for front-end file like images, stylesheets, JS files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
